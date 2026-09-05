444dcb066ebf60f491b711baea7a22a1fa7d575a
jkevinbaluyot <jkevinbaluyot@gmail.com>
2026-09-05 17:26:10 +0800

    Added route.js

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 const http = require('http');
+const routes = require('./routes');
 
-function rqListener(req, res){
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>My Website</title></head>')
-    res.write('<body><h1>Hello</h1></body>')
-    res.write('</html>')
-    res.end();
-}
-
-const server = http.createServer(rqListener);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
